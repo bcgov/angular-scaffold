732c75af0e4ec0456c44d9122648c6343a69a3d7
Greg Turner <greg.fun@gmail.com>
2017-07-11 10:57:17 -0700

    fixed e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('base-app2 App', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Theming your app');
   });
 });
