1c95f0791cb14e31926a24bf370076a7645cc682
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2021-01-18 10:31:43 -0800

    Dynamically retrieve config endpoint IP from envs (#30)

--- openshift/templates/nginx-runtime/fetch_env.js
+++ openshift/templates/nginx-runtime/fetch_env.js
@@ -1,3 +1,11 @@
 function fetch_config_endpoint(r) {
-  return process.env.CONFIG_ENDPOINT;
+  // CONFIG_ENDPOINT_HOST and CONFIG_ENDPOINT_PORT are names of the
+  // environment variables injected by kubernetes.  The actual values
+  // of those environment variables contain the IP and port
+  var endpoint = "http://";
+      endpoint += process.env[process.env.CONFIG_ENDPOINT_HOST] 
+      endpoint += ":" 
+      endpoint += process.env[process.env.CONFIG_ENDPOINT_PORT];
+
+  return endpoint;
 }
